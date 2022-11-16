f793f998e7f98939d7e11cdda7f3c5a4a97d2491
Ken_sir <KLOW024@e.ntu.edu.sg>
2022-11-16 15:58:39 +0800

    Glitchy working spi

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,8 +121,12 @@ int main(void)
 
 	  //3. Bring slave to select high
 	  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-    /* USER CODE END WHILE */
 
+
+    /* USER CODE END WHILE */
+	  //end of ADC polling
+	  HAL_Delay(0.01);
+	  HAL_ADC_Stop(&hadc1);
     /* USER CODE BEGIN 3 */
 
   }
@@ -272,12 +276,22 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
